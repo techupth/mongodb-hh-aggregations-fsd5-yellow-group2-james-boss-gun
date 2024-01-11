a28e6a7bc504a9df848e2130bdd1e9a08ee33d3b
BOSS <bigboss1796@hotmail.com>
2024-01-11 13:24:23 +0700

    feat:Aggregation

--- exercise-5.js
+++ exercise-5.js
@@ -1,1 +1,22 @@
 // Paste the complete MQL here
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: { month: "$created_at", year: "$created_at" },
+      total_sales: { $sum: "$total_price" },
+    },
+  },
+  { $sort: { _id: -1 } },
+]);
+
+หรือ;
+
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: { month: "$created_at", year: "$created_at" },
+      total_sales: { $sum: "$total_price" },
+    },
+  },
+  { $sort: { "-id_year": -1, "_id.month": -1 } },
+]);
--- exercise-7.js
+++ exercise-7.js
@@ -1,1 +1,11 @@
 // Paste the complete MQL here
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: { year: { $year: "$created_at" }, month: { $month: "$created_at" } },
+      total_price_by_month: { $sum: "$total_price" },
+    },
+  },
+  { $sort: { total_price_by_month: 1 } },
+  { $limit: 1 },
+]);
